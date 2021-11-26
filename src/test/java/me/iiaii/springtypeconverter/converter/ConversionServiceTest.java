829cfa9dcf3c010cfc35441dff2db143d4145cd2
iiaii <jhun9409@naver.com>
2021-11-26 23:48:32 +0900

    test, feat: conversionService 컨버팅 적용

---
+++ src/test/java/me/iiaii/springtypeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,29 @@
+package me.iiaii.springtypeconverter.converter;
+
+import me.iiaii.springtypeconverter.type.IpPort;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConversionServiceTest {
+
+    @Test
+    @DisplayName("conversionService")
+    void conversionService() {
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(IpPort.of("127.0.0.1", 8080));
+        assertThat(conversionService.convert(IpPort.of("127.0.0.1", 8080), String.class)).isEqualTo("127.0.0.1:8080");
+    }
+
+
+}
